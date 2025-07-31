10bb11fc88b36c9b2543b80b824521eba12d4c2d
Adison Dela Cruz <adisondc143@gmail.com>
2025-07-30 20:27:09 -0700

    walk up walls

--- player.gd
+++ player.gd
@@ -2,24 +2,42 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = 400.0
+const GRAVITY = 980
+
+var player_normal_vector
+var rotation_direction = 0
+
+func _ready() -> void:
+	player_normal_vector = Vector2.UP
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	var ground_normal_vector = $RayCast2D.get_collision_normal()
+	var rotation_change = player_normal_vector.angle_to(ground_normal_vector)
+	rotation_direction += rotation_change
+	rotation = rotation_direction
+	
+	set_up_direction(ground_normal_vector)
+	
+	# Handle gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+		#print("Above ground")
+		velocity += -ground_normal_vector * GRAVITY * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		#print("Jump action received")
+		#print(player_normal_vector * JUMP_VELOCITY)
+		velocity = player_normal_vector * JUMP_VELOCITY
+	
+	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = transform.x * direction * SPEED
+	
+	print(is_on_floor())
+	print(velocity)
 	move_and_slide()
+	
+	player_normal_vector = ground_normal_vector
+	
