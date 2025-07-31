8ec5177c09dc8dcd3b9fc352a48df28b3a85443c
Adison Dela Cruz <adisondc143@gmail.com>
2025-07-30 22:39:42 -0700

    Clean up out of painting movement

--- player.gd
+++ player.gd
@@ -1,43 +1,54 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = 400.0
-const GRAVITY = 980
+@export var SPEED: float = 300.0
+@export var JUMP_VELOCITY: float = 400.0
+@export var GRAVITY: float = 980
 
-var player_normal_vector
-var rotation_direction = 0
+var player_normal_vector: Vector2
+var velocity_drop: Vector2
+
+var is_airborne: bool
 
 func _ready() -> void:
 	player_normal_vector = Vector2.UP
+	velocity_drop = Vector2.ZERO
+	is_airborne = false
 
 
 func _physics_process(delta: float) -> void:
 	
 	var ground_normal_vector = $RayCast2D.get_collision_normal()
 	var rotation_change = player_normal_vector.angle_to(ground_normal_vector)
-	rotation_direction += rotation_change
-	rotation = rotation_direction
+	rotation += rotation_change
 	
 	set_up_direction(ground_normal_vector)
+	velocity = velocity_drop
 	
-	# Handle gravity
 	if not is_on_floor():
-		#print("Above ground")
-		velocity += -ground_normal_vector * GRAVITY * delta
+		is_airborne = true
+		velocity = velocity.rotated(rotation_change) + (-ground_normal_vector * GRAVITY * delta)
+	else:
+		is_airborne = false
+		velocity = -ground_normal_vector * GRAVITY
 	
-	if Input.is_action_just_pressed("jump"):
-		#print("Jump action received")
-		#print(player_normal_vector * JUMP_VELOCITY)
-		velocity = player_normal_vector * JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") && !is_airborne:
+		is_airborne = true
+		velocity = ground_normal_vector * JUMP_VELOCITY
+	
+	velocity_drop = velocity
 	
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity = transform.x * direction * SPEED
+		velocity = velocity_drop + (transform.x * direction * SPEED)
+		print(velocity_drop)
+		print(transform.x * direction * SPEED)
+		#print(velocity)
+	else:
+		velocity = velocity_drop
+		
 	
-	print(is_on_floor())
-	print(velocity)
-	move_and_slide()
-	
+	#print(velocity)
+
 	player_normal_vector = ground_normal_vector
-	
+	move_and_slide()
