c6cd15cfaa0b180a9d7df835c3a82c62c2dde055
Adison Dela Cruz <adisondc143@gmail.com>
2025-08-03 03:19:16 -0700

    Make fish disappear when hitting killzone

--- levels/debug_room/killzone.gd
+++ levels/debug_room/killzone.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal player_dead
+
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body):
 	print("1")
-	timer.start()	
+	player_dead.emit()
+	timer.start()
 
 func _on_timer_timeout(): 
 	get_tree().reload_current_scene()
